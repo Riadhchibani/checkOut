e89604b91f563468c29b8bd2ffe2b8134331036e
Riadhchibani <ryadhchibani16@outlook.com>
2023-10-26 13:19:34 +0200

    build web app

--- lib/profilePage/ProfilePage.dart
+++ lib/profilePage/ProfilePage.dart
@@ -1,9 +1,8 @@
 import 'package:app/homePage.dart/HomePage.dart';
 import 'package:app/messengerPage/MessengerApp.dart';
 import 'package:app/router/customRoutingApp.dart';
-//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
